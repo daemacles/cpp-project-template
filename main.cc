11fa0fce81435586a40a5f21450960d5a8b7a2e2
James Youngquist <jay@cs.uw.edu>
2015-02-19 00:49:18 -0800

    And more little tweaks so it compiles

--- main.cc
+++ main.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+  // DELETE THESE.  Used to suppress unused variable warnings.
+  (void)argc;
+  (void)argv;
+
   std::cout << "Hello, World!" << std::endl;
 
   return 0;
